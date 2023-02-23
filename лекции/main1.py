f618dfc9002ef2a90622e0d19c7675a91eea8a21
Anna Zuma <3umchik24@mail.ru>
2023-02-24 00:32:11 +0300

    уроки

---
+++ лекции/main1.py
@@ -0,0 +1,18 @@
+n = 5
+print(type(n))
+n = '3\'o3'
+print(n)
+print(type(n))
+
+a = 4
+b = 5.3334
+c = "uilhdu"
+print(a,' , ', b, ', ',c)
+print(f"{a} - {b} - {c}")
+print("{} - {} - {}".format(a,b,c))
+
+print("Введите")
+a = input()
+print(a)
+a = int(input("Введите:" ))
+print(a*a)
