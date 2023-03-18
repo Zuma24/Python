f38a7880e44bcc46fe031d59a58834a7b74a0963
Anna Zuma <3umchik24@mail.ru>
2023-03-19 01:22:17 +0300

    8

--- Дз/8/8.py
+++ Дз/8/8.py
@@ -12,9 +12,18 @@
 # Тучи разошлись
 # Листва зеленела
 
+def read_last(lines, file):
+    if lines != 0:
+        line = file.readlines()[-lines:]
+        for el in line:
+            print(el)
 
-
-
+n = int(input("Введите кол-во строк: "))
+if n >= 0 and n % 1 == 0:
+    with open('article.txt', 'r', encoding='utf-8') as file:
+        read_last(n, file)
+else:
+    print("Вы указали неверное значение")
 
 
 # Документ «article.txt» содержит следующий текст:
@@ -30,3 +39,24 @@
 # Требуется реализовать функцию longest_words(file),
 # которая записывает в файл result.txt слово, имеющее максимальную длину
 # (или список слов, если таковых несколько).
+def longest_words(file):
+    line = file.read().splitlines()
+    max = len(line[0])
+    result = []
+
+    for i in range(len(line)):
+        if len(line[i]) > max:
+            result = []
+            result.append(line[i])
+            max = len(line[i])
+        elif len(line[i]) == max:
+            result.append(line[i])
+    write_word(result)
+def write_word(result):
+    with open('result.txt', 'w', encoding='utf-8') as res:
+        for el in result:
+            res.write(el)
+
+
+with open('article.txt', 'r', encoding='utf-8') as file:
+    longest_words(file)
---
+++ Уроки/8.py
@@ -0,0 +1,25 @@
+# with open('article.txt', 'r', encoding='utf-8') as file:
+    # text = file.read()
+    # for letter in text:
+    #     print(letter)
+
+    # text = file.read().splitlines()
+    # print(text)
+
+    # text = file.readlines()
+    # print(text)
+
+
+    # line = file.readline()
+    # while line != '':
+    #     print(line)
+    #     line = file.readline()
+
+    # line = file.readline()
+    # while line:
+    #     print(line)
+    #     line = file.readline()
+
+
+    # symbol = input()
+    # print(file.read().count(symbol))
